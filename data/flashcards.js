09091b3b0576c4943656756a7621b71153f70d9b
Maciej Chojak <maciekch7991@gmail.com>
2021-03-22 19:13:35 +0100

    feature: add flashcards file

---
+++ data/flashcards.js
@@ -0,0 +1,22 @@
+export const flashcards = [
+    {
+        polish: 'kot',
+        german: 'die Katze',
+    },
+    {
+        polish: 'zaczynać',
+        german: 'anfagen',
+    },
+    {
+        polish: 'robotnik',
+        german: 'der Arbeiten',
+    },
+    {
+        polish: 'fotograf',
+        german: 'der Fotograf',
+    },
+    {
+        polish: 'biurko',
+        german: 'die Schreibtisch',
+    },
+]
