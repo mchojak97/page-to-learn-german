6f291585d04f5994d8a1dea2cffe48bbcc13d774
Maciej Chojak <maciekch7991@gmail.com>
2021-03-24 00:58:31 +0100

    refactor: add new flashcards and phrases

--- data/flashcardsList.js
+++ data/flashcardsList.js
@@ -19,4 +19,64 @@ export const flashcardsList = [
         polish: 'biurko',
         german: 'die Schreibtisch',
     },
+    {
+        polish: 'szynka',
+        german: 'der Schinken',
+    },
+    {
+        polish: 'kiełbasa',
+        german: 'die Wurst',
+    },
+    {
+        polish: 'sól',
+        german: 'das Salz',
+    },
+    {
+        polish: 'mięso',
+        german: 'das Fleisch',
+    },
+    {
+        polish: 'ryż',
+        german: 'der Reis',
+    },
+    {
+        polish: 'ziemniaki',
+        german: 'die Kartoffeln',
+    },
+    {
+        polish: 'makaron',
+        german: 'die Nudeln',
+    },
+    {
+        polish: 'ciasto',
+        german: 'der Kuchen',
+    },
+    {
+        polish: 'lody',
+        german: 'das Eis',
+    },
+    {
+        polish: 'mleko',
+        german: 'die Milch',
+    },
+    {
+        polish: 'cukier',
+        german: 'der Zucker',
+    },
+    {
+        polish: 'herbata',
+        german: 'der Tee',
+    },
+    {
+        polish: 'kawa',
+        german: 'der Kaffe',
+    },
+    {
+        polish: 'woda',
+        german: 'das Wasser',
+    },
+    {
+        polish: 'garnek',
+        german: 'der Topf',
+    },
 ]
